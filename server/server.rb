77ebe4e873986a341fd1a2d849c5d70483b489ff
jesuslerma <shuyojl@gmail.com>
2018-07-02 22:18:41 -0500

    Add client and server code

---
+++ server/server.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+
+file_path = '../client/index.html'
+
+get "/" do
+	file_name = "../client/index.html"
+	file = File.open(file_name, "r")
+	html = file.read
+	file.close
+	html
+end
+
+get "/style.css" do
+	file_name = "../client/style.css"
+	file = File.open(file_name, "r")
+	css = file.read
+	file.close
+	
+	content_type 'text/css'
+	css
+end
